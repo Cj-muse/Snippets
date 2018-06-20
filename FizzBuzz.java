2a62b90c84938b42770274dfca9a7598fb82795c
connermattingly <connermattingly@yahoo.com>
2018-06-20 11:08:28 -0400

    Implemented Kata

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,13 +2,45 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class FizzBuzz {
-
-    public String FizzBuzz(int input){
-      return "1";
-    }
   
-    @Test
-    public void multiplicationOfZeroIntegersShouldReturnZero() {
-       assertEquals("1", FizzBuzz(1));
+  public String FizzBuzz(int input){
+   if (input % 15 == 0) {
+      return "FizzBuzz";
+    }    
+    if (input % 5 == 0) {
+      return "Buzz";
+    }
+    if (input % 3 == 0) {
+      return "Fizz";
+    }
+    return Integer.toString(input);      
+  }
+  
+  @Test
+  public void ShouldReturnFizzBuzzGivenMultiplesOfFifteen() {
+    assertEquals("FizzBuzz", FizzBuzz(15));
+    assertEquals("FizzBuzz", FizzBuzz(30));
+    assertEquals("FizzBuzz", FizzBuzz(45));
+  }
+  
+  @Test
+  public void ShouldReturnBuzzGivenMultiplesOfFive() {
+     assertEquals("Buzz", FizzBuzz(5));
+     assertEquals("Buzz", FizzBuzz(10));
+     assertEquals("Buzz", FizzBuzz(20));
+  }
+  
+  @Test
+  public void ShouldReturnFizzGivenMultiplesOfThree() {
+     assertEquals("Fizz", FizzBuzz(3));
+    assertEquals("Fizz", FizzBuzz(6));
+    assertEquals("Fizz", FizzBuzz(9));
+  }
+
+  @Test
+    public void shouldReturnInput() {
+      assertEquals("1", FizzBuzz(1));
+      assertEquals("2", FizzBuzz(2));
+      assertEquals("4", FizzBuzz(4));
     }
 }
